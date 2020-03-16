d2cdcfe2f94ee7a7ba2e69151349a4773770b1b8
Hasaber8 <rohanhasabe8@gmail.com>
2020-03-16 21:50:47 +0530

    chef: Init PE

--- aosp_chef.mk
+++ aosp_chef.mk
@@ -14,16 +14,19 @@
 # limitations under the License.
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Aosp stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Device
 $(call inherit-product, device/motorola/chef/device.mk)
+
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -61,9 +64,9 @@ TARGET_RECOVERY_FSTAB := device/motorola/sdm660-common/rootdir/etc/fstab.qcom
 
 # Device identifiers
 PRODUCT_DEVICE := chef
-PRODUCT_NAME := lineage_chef
+PRODUCT_NAME := aosp_chef
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Motorola One Power
+PRODUCT_MODEL := One Power
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := chef
 
